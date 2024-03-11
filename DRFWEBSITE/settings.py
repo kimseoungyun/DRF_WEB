035314e85943868e9f8d3cf0033e5af4d4592fbb
kimseongyun <701ksy@gmail.com>
2024-03-11 20:07:41 +0900

    helo

--- DRFWEBSITE/settings.py
+++ DRFWEBSITE/settings.py
@@ -49,7 +49,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '127.0.0.1', '.localhost']
 
 
 # Application definition
